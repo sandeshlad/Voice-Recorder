7b1cf3d5c94e895468ed37eed876a69eccadb6bc
DELL <sandeshlad67@gmail.com>
2021-02-03 16:57:01 +0530

    Voice Recorder..

---
+++ app/src/main/java/com/sandy/example/voicerecorder/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sandy.example.voicerecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
